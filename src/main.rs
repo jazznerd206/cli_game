864a814a408160cd2877d38c6db8171b1b87f16e
Andrew Miller <206alm@gmail.com>
2021-02-26 12:53:31 -0800

    cli game from docs up to date

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,19 @@ fn main() {
     println!("Welcome to the thunderdome, bitch!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);
-    
-    println!("Guess a number between 1 and 100!");
     
     loop {
-        println!("Please input your guess.");
+        println!("Guess a number between 1 and 100!");
         
         let mut guess = String::new();
     
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
     
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
