cba8ec994fb692a953040d6a63071feb9df88052
Andrew Miller <206alm@gmail.com>
2021-02-26 12:27:00 -0800

    add lock file and random library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number between 1 and 100!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
